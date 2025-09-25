5963ca4b49264395cc7a846c22100eda76ef0676
Thi Nguyen <ririchiroyo@csu.fullerton.edu>
2025-09-25 13:25:03 -0700

    added models for owners and sitters

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ("OWNER", "Owner"),
+        ("SITTER", "Sitter"),
+    )
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default="OWNER")
+    is_verified = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.username} ({self.role})"
