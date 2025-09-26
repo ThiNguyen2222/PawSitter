db61ecd1f22d1f46b9012525dc973f9fb536670e
ngocpham3499 <111798812+ngocpham3499@users.noreply.github.com>
2025-09-25 22:08:37 -0700

    Tailwind added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcssVite from '@tailwindcss/vite'; // import the Tailwind Vite plugin
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    tailwindcssVite() // add the plugin here
+  ],
+});
