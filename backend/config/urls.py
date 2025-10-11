7c5325c8051829c1aa45908ec6e1f1be7a9724af
Helen Truong <htruong122@csu.fullerton.edu>
2025-10-10 17:28:12 -0700

    temporary check to confirm our backend responding
    created a config/views.py file so when we run the server we know its working properly.

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -18,8 +18,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from . import views
 
 urlpatterns = [
+    path("", views.index, name="index"),
     path("admin/", admin.site.urls),
 
     # auth (JWT)
