dd39c8272775ed587fdc31f413457558d34d5631
Helen Truong <htruong122@csu.fullerton.edu>
2025-10-20 15:24:47 -0700

    backend/accounts.admin.py
    Edited the file, when i go into my superuser account I'm able to see accounts.

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+User = get_user_model()
+
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    list_display = ("username", "email", "role", "is_verified", "is_active", "is_staff")
+    list_filter  = ("role", "is_verified", "is_staff", "is_superuser", "is_active", "groups")
+    search_fields = ("username", "email", "first_name", "last_name")
+    ordering = ("username",)
+
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email")}),
+        (_("PawSitter"), {"fields": ("role", "is_verified")}),  # custom fields
+        (_("Permissions"), {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("username", "email", "password1", "password2", "role", "is_verified"),
+        }),
+    )
