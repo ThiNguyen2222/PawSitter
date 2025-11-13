38efa4b42e82e7c98b532782f7d46d47f13c89cd
Helen Truong <htruong122@csu.fullerton.edu>
2025-11-12 17:32:59 -0800

    update profile

--- frontend/src/pages/sitter/Profile.jsx
+++ frontend/src/pages/sitter/Profile.jsx
@@ -3,8 +3,6 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import ResponsiveMenu from "../../components/ResponsiveMenu";
 import { getMySitterProfile } from "../../api/api";
-// NOTE: utils is under owner/dashboard in your structure
-import { getSitterImage, getPetImage } from "../owner/dashboard/utils";
 import pawIcon from "../../assets/images/paw.png";
 console.log("paw icon path:", pawIcon);
 
